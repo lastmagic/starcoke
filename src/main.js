2000fe1976b09b3c0d7537a8f7ac190887ab4fae
gv0413 <ggvv0413@gmail.com>
2020-04-18 23:15:35 +0900

    jwt 모듈 추가

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import BigNumber from 'bignumber.js'
+import VueJWT from 'vuejs-jwt'
 import './assets/css/luniverstar.css'
 import './assets/vendor/css/bootstrap.css'
 
 Vue.use(VueAxios, axios)
 Vue.use(BigNumber)
+Vue.use(VueJWT, {
+  signKey: 'tmpEncryptKey'
+})
 
 Vue.config.productionTip = false
 
