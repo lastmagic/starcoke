e0ecfb9e8a7fc7f874943d998841516b8ac2c575
gv0413 <ggvv0413@gmail.com>
2020-04-20 17:13:02 +0900

    vue config public opt 추가

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   devServer: {
-      proxy: {
-          '/api': {
-              target: 'http://localhost:3000',
-              changeOrigin: true,
-              pathRewrite: {
-                  '^/api': ''
-              }
-          }
-      }
+		public: 'starcoke.kr',
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': ''
+				}
+			}
+		}
   }
 }
